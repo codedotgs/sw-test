bc380fdc218fd6bb99fb5a0f7f42607a3830ef56
codedotgs <codedotgs@gmail.com>
2020-02-24 09:12:07 +0100

    added boxShadow on nav

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import '../src/style.css';
 import '../src/style.mobile.css';
 
 
-// const animate =
-
-
 let element = document.querySelector('nav');
 let menu = document.querySelector('#open');
 let body = document.querySelector('body');
@@ -13,10 +10,14 @@ let close = document.querySelector('#close');
 menu.addEventListener('click', event => {
   element.style.animation = "openNav 1s 1";
   element.style.left = "0vw";
+  element.style.boxShadow = "10px 10px 20px 12px black";
   body.style.overflow = "hidden";
 });
 close.addEventListener('click', event => {
   element.style.animation = "closeNav 1s 1";
   element.style.left = "100vw";
   body.style.overflow = "scroll";
+  element.style.boxShadow = "none";
 });
+
+
